87fc187474e2255e3d69b9608d99752ae27151fa
admin-ПК\admin <metallik91@mail.ru>
2019-11-14 21:46:44 +0300

    some edit

--- Books/Controllers/HomeController.cs
+++ Books/Controllers/HomeController.cs
@@ -22,20 +22,5 @@ namespace Tele.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
