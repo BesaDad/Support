95d28bb227d8771b4412d45e3da0044a8e809f9a
admin-ПК\admin <metallik91@mail.ru>
2019-11-18 10:23:59 +0300

    delete home controller

--- Books/App_Start/RouteConfig.cs
+++ Books/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tele
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Tele", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
